fad41789343fc00deb96940f8121fb8807bd4cc3
wangyixian <ouitiken@gmail.com>
2016-04-11 00:00:20 +0800

    修改了一些文字描述

--- js/common.js
+++ js/common.js
@@ -1,5 +1,6 @@
 /**
  * Created by Ethan on 16/4/6.
  */
-var server = "http://localhost:63342/iidooo-homepage";
+//var server = "http://localhost:63342/iidooo-homepage";
+var server = "http://www.iidooo.com";
 var activeNavItem = "index";
